570fbb00bc3277a1bf77a41588e1ff4b42ba7798
Justin Stuck <justin.stuck@gmail.com>
2023-12-03 00:46:10 -0600

    only errors in linting for now

--- courier/config.py
+++ courier/config.py
@@ -1,11 +1,11 @@
 import os
 
 
-class Config(object):
+class Config:
     @property
-    def sender_email_address(self):
+    def sender_email_address(self) -> str:
         return os.environ['sender_email_address']
 
     @property
-    def sender_email_password(self):
+    def sender_email_password(self) -> str:
         return os.environ['sender_email_password']
